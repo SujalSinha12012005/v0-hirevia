0645ed2133c69a24439d457291eb0ad904c1c595
SujalSinha12012005 <sahupankaj1034@gmail.com>
2026-04-09 10:18:03 +0530

    fixed vercel links

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,6 +8,7 @@ export function getURL() {
   let url =
     process.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production
     process.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set on Vercel
+    process.env?.VERCEL_URL ?? // Automatically set on Vercel (server-side)
     'http://localhost:3000/'
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`
